6affddac6645aa0b018b2aaffd056dd77eabeaff
shforeal <rivena14798@gmail.com>
2023-04-07 09:21:20 +0300

    feat: post api , and test

--- flask_app/routes/gpt_routes.py
+++ flask_app/routes/gpt_routes.py
@@ -1,7 +1,19 @@
-from flask import Blueprint
-
+from flask import Blueprint, request, jsonify
+from ..services.gpt_service import get_gpt_response
 gpt_routes = Blueprint('gpt_routes', __name__)
 
 @gpt_routes.route('/', methods=['GET'])
 def gpt():
-    return "Welcome to ChatGPT API Wrapper!" #render_template("gpt.html", user=current_user)
+    return "Welcome to ChatGPT API Wrapper!" 
+
+
+@gpt_routes.route('/chat', methods=['POST'])
+def send_message():
+    data = request.get_json()
+    message = data.get('message', 'No message found in JSON object')
+    gpt_res = get_gpt_response(message)
+    response = {
+        'status': 'success',
+        'received_message_from_gpt': gpt_res
+    }
+    return jsonify(response)
